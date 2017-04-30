d3884498eb6504440fa13f574f8f7391bd115c7e
Corey <cry@Coreys-Air.attlocal.net>
2017-04-30 14:10:29 -0700

    fleshed out table view and made tests

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -76,7 +76,7 @@ class TableModel {
 module.exports = TableModel;
 },{}],5:[function(require,module,exports){
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTR, createTH, createTD } = require('./dom-util');
 
 class TableView {
 	constructor(model) {
@@ -90,10 +90,12 @@ class TableView {
 
 	initDomReferences() {
 		this.headerRowEl = document.querySelector('THEAD TR');
+		this.sheetBodyEl = document.querySelector('TBODY');
 	}
 
 	renderTable() {
 		this.renderTableHeader();
+		this.renderTableBody();
 	}
 
 	renderTableHeader() {
@@ -104,6 +106,22 @@ class TableView {
 		  .map(colLabel => createTH(colLabel))
 		  .forEach(th => this.headerRowEl.appendChild(th));
 	}	
+
+	renderTableBody() {
+		const fragment = document.createDocumentFragment();
+		for (let row = 0; row < this.model.numRows; row++) {
+			const tr = createTR();
+			for (let col = 0; col < this.model.numCols; col++) {
+				const position = {col: col, row: row};
+				const value = this.model.getValue(position);
+				const td = createTD(value);
+				tr.appendChild(td);
+			}
+			fragment.appendChild(tr);
+		}
+		removeChildren(this.sheetBodyEl);
+		this.sheetBodyEl.appendChild(fragment);
+	}
 }
 
 module.exports = TableView;
